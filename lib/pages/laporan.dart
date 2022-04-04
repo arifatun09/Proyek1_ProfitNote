a4d4f6219b595eb0e730cd63ee075c5331215246
RadenDimasErlangga21 <zirkified123@gmail.com>
2022-04-04 21:51:48 +0700

    penambahan menu laporan

--- lib/pages/laporan.dart
+++ lib/pages/laporan.dart
@@ -7,7 +7,71 @@ class Laporan extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Laporan'),
+        title: const Text('Laporan Untung Rugi'),
+      ),
+    body: Column(
+        children: [
+          Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/modal');
+              },
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/images/laporanuntung.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Laporan Untung Rugi',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/lihat_grafik_untungrugi');
+              },
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: Image(
+                        image: AssetImage('assets/images/grafik.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Lihat Grafik Untung Rugi',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
