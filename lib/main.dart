6da92c211d7b58401eeec87b72ba5cb7c88aa776
cellinesalsabiela <cellinesalsabiela574@gmail.com>
2022-04-04 21:21:48 +0700

    Menambahkan halaman pembukuan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pages/modal.dart';
+import 'pages/pengeluaran_pemasukan.dart';
 import 'pages/home_page.dart';
 import 'pages/laporan.dart';
 import 'pages/pembukuan.dart';
@@ -10,6 +12,8 @@ void main() {
       '/': (context) => const HomePage(),
       '/pembukuan': (context) => const Pembukuan(),
       '/laporan': (context) => const Laporan(),
+      '/modal': (context) => const Modal(),
+      '/pengeluaran_pemasukan':(context) => const Pengeluaran_Pemasukan(),
     },
   ));
 }
---
+++ lib/pages/pengeluaran_pemasukan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Pengeluaran_Pemasukan extends StatelessWidget {
+  const Pengeluaran_Pemasukan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input Pengeluaran dan Pemasukkan'),
+      ),
+    );
+  }
+}
