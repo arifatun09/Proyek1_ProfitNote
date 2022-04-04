5461a77dc3d4c954a2dc2ae43899fcb1bf7fffa8
RadenDimasErlangga21 <zirkified123@gmail.com>
2022-04-04 22:08:40 +0700

    penambahan menu laporan dan grafik

--- lib/pages/laporan_untung_rugi.dart
+++ lib/pages/laporan_untung_rugi.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Modal extends StatelessWidget {
-  const Modal({Key? key}) : super(key: key);
+class LaporanUntungRugi extends StatelessWidget {
+  const LaporanUntungRugi({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
